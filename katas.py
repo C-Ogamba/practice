e7fe3a0914aadbc36bcce050ce8fa217fffc718f
Kemunto <cin.kemu@gmail.com>
2022-09-22 23:49:53 +0300

    get solution

--- katas.py
+++ katas.py
@@ -5,3 +5,12 @@ phoneBook = {}
 for i in range(n):
     contact = input().split(' ')
     phoneBook[contact[0]] = contact[1]
+
+# Process Queries
+lines = sys.stdin.readlines()
+for i in lines:
+    name = i.strip()
+    if name in phoneBook:
+        print(name + '=' + str( phoneBook[name] ))
+    else:
+        print('Not found')
