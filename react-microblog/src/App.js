350c14c7e8a0dbe38caa209ec7a3b412e4e09107
C-Ogamba <cin.kemu@gmail.com>
2022-10-01 22:58:21 +0300

    Update the microblog page and restructure

--- react-microblog/src/App.js
+++ react-microblog/src/App.js
@@ -23,21 +23,25 @@ export default function App() {
 
   return (
     <Container fluid className="App">
-      
-      <h1>Microblog</h1>
-      {posts.length === 0 ?
-        <p>There are no blog posts.</p>
-      :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
-      }
+        <Header/>
+        <Container>
+          <h1>Microblog</h1>
+          {posts.length === 0 ?
+            <p>There are no blog posts.</p>
+          :
+            posts.map(post => {
+              return (
+                <p key={post.id}>
+                  <b>{post.author.username}</b> &mdash; {post.timestamp}
+                  <br />
+                  {post.text}
+                </p>
+              );
+            })
+          }
+
+        </Container>
+
     </Container>
   );
 }
